3028a82041597bfbc67dd6a85fb4e346e456dfb3
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-08-03 23:16:42 -0300

    reexport things

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Upload: require('./Upload.js'),
+  GraphQLUpload: require('./GraphQLUpload.js'),
+  graphqlUploadKoa: require('./graphqlUploadKoa.js'),
+  graphqlUploadExpress: require('./graphqlUploadExpress.js'),
+};
